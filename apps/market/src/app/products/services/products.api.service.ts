e4b5bcb87e40f67c3a5180909bf1e9a8e1300031
Maksim Chernov <maksche0@gmail.com>
2024-04-27 14:54:53 +0300

    initial commit

--- apps/market/src/app/products/services/products.api.service.ts
+++ apps/market/src/app/products/services/products.api.service.ts
@@ -10,6 +10,6 @@ export class ProductsApiService {
   }
 
   fetchProducts(): Observable<Product[]> {
-    return this._http.get<Product[]>('/assets/mocks/products.json');
+    return this._http.get<Product[]>('/joker-87.github.io/assets/mocks/products.json');
   }
 }
--- preview/market/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-PMFLDNSK.mjs";import"./chunk-T7LKHE4D.mjs";import"./chunk-WEE3QU2K.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
